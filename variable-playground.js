390bf71e7db808313e22c72e2bd013a939a30e02
Charith Perera <charith.p@gmail.com>
2016-07-31 22:31:37 +1000

    Add variable playground

---
+++ variable-playground.js
@@ -0,0 +1,30 @@
+// var person = {
+// 	name: "Andrew",
+// 	age: 21
+// };
+
+// function updatePerson(pers) {
+// 	// pers = {
+// 	// 	name: "Andrew",
+// 	// 	age: 24
+// 	// }
+
+// 	pers.age = 23;
+// }
+
+// updatePerson(person);
+// console.log(person);
+
+var grades = [15, 27];
+
+function addGrade(arr) {
+	//arr.push(69);
+
+	arr = [15, 27, 69]
+	debugger;
+}
+
+addGrade(grades);
+console.log(grades);
+
+
